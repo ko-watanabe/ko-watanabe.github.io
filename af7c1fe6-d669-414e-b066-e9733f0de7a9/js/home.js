6ebb8958d346fda61a33d629d324bfca7c0f6686
ko-watanabe <ko.watanabe.0522@gmail.com>
2023-06-29 17:18:31 +0200

    Change website

---
+++ af7c1fe6-d669-414e-b066-e9733f0de7a9/js/home.js
@@ -0,0 +1,9 @@
+function search() {
+  console.log("pressed button");
+  document.getElementById("search_result_1").hidden = true;
+  document.getElementById("search_result_2").hidden = true;
+  document.getElementById("search_result_3").hidden = true;
+  document.getElementById("search_result_4").hidden = true;
+  document.getElementById("search_result_5").hidden = true;
+  document.getElementById("search_result_6").hidden = true;
+}
